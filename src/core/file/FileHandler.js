69a9be36c9a7eac3391f588e11b51631fbfa2f35
Richard Regeer <rich2309@gmail.com>
2019-08-14 22:53:57 +0200

    Add tested FileHandler

--- src/core/file/FileHandler.js
+++ src/core/file/FileHandler.js
@@ -4,11 +4,13 @@ const fs = require('fs');
 
 class FileHandler {
   writeFileSync(fileName, path, contents) {
+    const fullPath = this._getFullPath(fileName, path);
+
     if (!fs.existsSync(path)) {
-      // TODO create directory
+      fs.mkdirSync(path, { recursive: true });
     }
 
-    fs.writeFileSync(this._getFullPath(fileName, path), contents);
+    fs.writeFileSync(fullPath, contents);
   }
 
   copyFileSync(fileName, source, destination) {
@@ -16,11 +18,11 @@ class FileHandler {
     const fullDestinationPath = this._getFullPath(fileName, destination);
 
     if (!fs.existsSync(fullSourcePath)) {
-      throw new Error(`Unable to copy file ${fullSourcePath}, does not exsists`);
+      throw new Error(`Unable to copy file ${fileName} from ${source}, it does not exists`);
     }
 
     if (!fs.existsSync(destination)) {
-      // TODO create directory
+      fs.mkdirSync(destination, { recursive: true });
     }
 
     fs.copyFileSync(fullSourcePath, fullDestinationPath);
@@ -30,7 +32,7 @@ class FileHandler {
     const fullPath = this._getFullPath(fileName, path);
 
     if (!fs.existsSync(fullPath)) {
-      throw new Error(`File ${fullPath} does not exsists`);
+      throw new Error(`File ${fileName} does not exsists`);
     }
 
     return fs.readFileSync(this._getFullPath(fileName, path)).toString();
--- tests/core/file/fileHandlerTest.js
+++ tests/core/file/fileHandlerTest.js
@@ -1,7 +1,111 @@
 'use strict';
 
 const test = require('ava');
+const fs = require('fs');
+const shell = require('shelljs');
 
-test('something', (t) => {
-  t.pass();
+const FileHandler = require('../../../src/core/file/FileHandler');
+
+test.before((t) => {
+  t.context.fileHandler = new FileHandler();
+
+  shell.mkdir('-p', '.test/new-destination');
+  shell.touch('.test/source-file.txt');
+  shell.echo('Some test data').to('.test/read-file.txt');
+});
+
+test.after(() => {
+  shell.rm('-rf', '.test');
+});
+
+test('WriteFile should write to file on the given path', (t) => {
+  const { fileHandler } = t.context;
+
+  fileHandler.writeFileSync(
+    'write-file.txt',
+    shell.pwd() + '/.test',
+    'some test data'
+  );
+
+  const path = '.test/write-file.txt';
+  const result = shell.find(path);
+
+  t.is(result[0], path);
+});
+
+test('WriteFile create the destination directory when it does not exists', (t) => {
+  const { fileHandler } = t.context;
+
+  fileHandler.writeFileSync(
+    'write-file.txt',
+    shell.pwd() + '/.test/write-destination',
+    'some test data'
+  );
+
+  const path = '.test/write-destination/write-file.txt';
+  const result = shell.find(path);
+
+  t.is(result[0], path);
+});
+
+test('CopyFile should copy a file from source to the given destination', (t) => {
+  const { fileHandler } = t.context;
+
+  fileHandler.copyFileSync(
+    'source-file.txt',
+    shell.pwd() + '/.test',
+    shell.pwd() + '/test/new-destination'
+  );
+
+  const result = shell.find('./test/new-destination/source-file.txt');
+
+  t.is(result.length, 1);
+});
+
+test('CopyFile should raise an error when the source file does not exists', (t) => {
+  const { fileHandler } = t.context;
+
+  t.throws(() => {
+    fileHandler.copyFileSync(
+      'unknowfile.txt',
+      shell.pwd() + '/.test',
+      shell.pwd() + '/test/new-destination'
+    );
+  }, Error);
+});
+
+test('CopyFile should create the destination directory when it does not exists', (t) => {
+  const { fileHandler } = t.context;
+
+  fileHandler.copyFileSync(
+    'source-file.txt',
+    shell.pwd() + '/.test',
+    shell.pwd() + '/test/other/destination'
+  );
+
+  const result = shell.find('./test/other/destination/source-file.txt');
+
+  t.is(result.length, 1);
+});
+
+test('Readfile should return the content of the file', (t) => {
+  const { fileHandler } = t.context;
+
+  const result = fileHandler.readFileSync(
+    'read-file.txt',
+    shell.pwd() + '/.test'
+  );
+
+  t.is(result, 'Some test data\n');
+});
+
+test('ReadFile should raise an error when the file does not exists', (t) => {
+  const { fileHandler } = t.context;
+
+  t.throws(() => {
+    fileHandler.readFileSync(
+      'unknowfile.txt',
+      shell.pwd() + '/.test'
+    );
+  }, Error);
 });
